0903ec0c4f1e089525f2afaf68e4dc7c23e8d518
metamuffin <yvchraiqi@protonmail.com>
2022-03-04 18:18:38 +0100

    a

--- index.ts
+++ index.ts
@@ -1,18 +1,24 @@
 import { states, createServer, createClient, Client, ServerClient, ServerOptions } from 'minecraft-protocol'
-import * as json5 from 'json5'
-import { readFileSync } from 'node:fs'
+
+export interface Config {
+    motd: string,
+    backend_port: number,
+    version: string
+    whitelist: { name: string, token?: string }[]
+}
+const config = require("./config.json") as Config
 
 const connectionOptions = {
-    host: "localhost",
-    port: 25565,
-    version: "1.18.1",
+    host: "127.0.0.1",
+    port: config.backend_port,
+    version: config.version,
 }
 
 const ChatMessage = require('prismarine-chat')(connectionOptions.version)
 const { MessageBuilder } = require('prismarine-chat')(connectionOptions.version)
 const mcData = require('minecraft-data')(connectionOptions.version)
 
-console.log("starting proxy instance...");
+console.log("starting");
 
 const server_options: ServerOptions = {
     host: "0.0.0.0",
@@ -22,20 +28,20 @@ const server_options: ServerOptions = {
     beforePing: function (response, client) {
         response.players.online = Math.floor(Math.random() * 1000);
         response.players.max = Math.floor(Math.random() * 1000);
-    }
+    },
 }
 
 const offline_server = createServer({
     ...server_options,
     port: 25566,
-    motd: "reverse proxy for offline auth",
+    motd: config.motd + "(token auth)",
     'online-mode': false,
 })
 
 const online_server = createServer({
     ...server_options,
     port: 25567,
-    motd: "reverse proxy for online auth",
+    motd: config.motd + "(mojang/microsoft auth)",
     'online-mode': true,
 })
 
@@ -65,9 +71,19 @@ function login_handler(client: Client, online: boolean) {
         if (!endedTargetClient) { targetClient.end('Error') }
     })
 
+    let username = client.username
+    if (!online) {
+        const kl = offline_auth(username)
+        if (!kl) return client.end("auth failed")
+        chat_log(client, "offline auth successful")
+        username = kl
+    } else {
+        chat_log(client, "online auth successful")
+    }
+
     const targetClient = createClient({
         ...connectionOptions,
-        username: "blub",
+        username,
         keepAlive: false,
         skipValidation: true
     })
@@ -78,19 +94,13 @@ function login_handler(client: Client, online: boolean) {
         console.log(`Connected client ${addr} to ${connectionOptions.host} as ${username}`)
     })
 
-    client.on('packet', function (data, meta) {
+    client.on('packet', (data, meta) => {
         if (targetClient.state === states.PLAY && meta.state === states.PLAY) {
             if (!endedTargetClient) {
                 if (meta.name === 'chat') {
-                    console.log(data);
-                    const message: string = data.message;
-                    if (message.indexOf("/,") === 0) {
-                        try {
-                            let evaled = eval(message.substring(3));
-                            console.log(evaled);
-                        } catch (e) {
-                            console.error(e);
-                        }
+                    let message: string = data.message
+                    if (message.startsWith("@")) {
+                        chat_log(client, message.substring(1))
                         return
                     }
                 }
@@ -141,9 +151,8 @@ function login_handler(client: Client, online: boolean) {
     })
 }
 
-function chat_log(m: string, c: Client) {
-    const message = MessageBuilder.fromString(`&8[&trash-auth&8] &7${m}`);
-    //@ts-ignore
+function chat_log(c: Client, m: string) {
+    const message = MessageBuilder.fromString(`&8[&atrash-auth&8] &7${m}`);
     c.write('chat', { message: JSON.stringify(message), position: 0, sender: '0' });
 }
 
